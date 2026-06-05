98904aed08101bf2e4167f23e545ec0d9df163c8
Subhan Ali <subhanch070@gmail.com>
2026-06-05 22:46:28 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ app.use(cors());
 
 const quotes = [
   { author: "Albert Einsteinn", quote: "Life is like riding a bicycle. To keep your balance, you must keep moving." },
-  { author: "Steve Jobs", quote: "Stay hungry, stay foolish." },
-  { author: "Nelson Mandela", quote: "It always seems impossible until it’s done." },
-  { author: "Muhammad Ali", quote: "Don’t count the days, make the days count." },
-  { author: "Abraham Lincoln", quote: "Whatever you are, be a good one." },
-  { author: "Mark Zuckerberg", quote: "The biggest risk is not taking any risk." },
+  { author: "Steve Jobss", quote: "Stay hungry, stay foolish." },
+  { author: "Nelson Mandelaa", quote: "It always seems impossible until it’s done." },
+  { author: "Muhammad Alii", quote: "Don’t count the days, make the days count." },
+  { author: "Abraham Lincolnn", quote: "Whatever you are, be a good one." },
+  { author: "Mark Zuckerbergg", quote: "The biggest risk is not taking any risk." },
   { author: "Bill Gates", quote: "Success is a lousy teacher." },
-  { author: "Elon Musk", quote: "When something is important enough, you do it even if the odds are not in your favor." },
-  { author: "Imran Khan", quote: "Never give up, no matter how hard life gets." },
-  { author: "Bruce Lee", quote: "Knowing is not enough, we must apply." }
+  { author: "Elon Muskk", quote: "When something is important enough, you do it even if the odds are not in your favor." },
+  { author: "Imran Khann", quote: "Never give up, no matter how hard life gets." },
+  { author: "Bruce Leee", quote: "Knowing is not enough, we must apply." }
 ];
 
 app.get("/api/quote", (req, res) => {
